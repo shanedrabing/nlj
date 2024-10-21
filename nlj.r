01888789ddec0333b7fc970003836e6f0f839a83
Shane Drabing <shane.drabing@gmail.com>
2024-10-21 16:43:59 -0600

    machine learning?

--- nlj.r
+++ nlj.r
@@ -21,6 +21,10 @@ rjohnson <- function(n, gamma = 0, delta = 1, xi = 0, lambda = 1) {
     lambda * sinh((qnorm(runif(n)) - gamma) / delta) + xi
 }
 
+gasinh <- function(q, gamma = 0, delta = 1, xi = 0, lambda = 1) {
+    gamma + delta * asinh((q - xi) / lambda)
+}
+
 
 # NORMALIZATION
 
@@ -39,39 +43,37 @@ znorm <- function(x) {
     # return
     list(normalize = normalize,
          denormalize = denormalize,
-         mean = mu,
-         sd = sigma,
+         par = c(mu, sigma),
          x = x,
          z = normalize(x))
 }
 
 zjohnson <- function(x) {
-    # default parameters
+    # parameterization (Q-Q)
     gdxl <- c(gamma = 0, delta = 1, xi = 0, lambda = 1)
-
-    # parameterization
     opt <- optim(gdxl, function(gdxl) {
-        z <- qnorm(pjohnson(x, gdxl[1], gdxl[2], gdxl[3], gdxl[4]))
-        suppressWarnings(ks.test(z, "pnorm")$p.value)
+        p <- pjohnson(x, gdxl[1], gdxl[2], gdxl[3], gdxl[4])
+        sum(abs(sort(p) - seq(0, 1, length.out = length(p))))
     })
 
-    # binding
+    # parameterization (K-S test)
     gdxl <- opt$par
-    zn <- znorm(qnorm(pjohnson(x, gdxl[1], gdxl[2], gdxl[3], gdxl[4])))
+    opt <- optim(gdxl, function(gdxl) {
+        p <- pjohnson(x, gdxl[1], gdxl[2], gdxl[3], gdxl[4])
+        suppressWarnings(ks.test(qnorm(p), "pnorm")$statistic)
+    })
 
-    # functions
+    # function definitions
+    gdxl <- opt$par
     normalize <- function(q)
-        zn$normalize(qnorm(pjohnson(q, gdxl[1], gdxl[2], gdxl[3], gdxl[4])))
+        qnorm(pjohnson(q, gdxl[1], gdxl[2], gdxl[3], gdxl[4]))
     denormalize <- function(z)
-        qjohnson(pnorm(zn$denormalize(z)), gdxl[1], gdxl[2], gdxl[3], gdxl[4])
+        qjohnson(pnorm(z), gdxl[1], gdxl[2], gdxl[3], gdxl[4])
 
     # return
     list(normalize = normalize,
          denormalize = denormalize,
-         gamma = gdxl[1],
-         delta = gdxl[2],
-         xi = gdxl[3],
-         lambda = gdxl[4],
+         par = opt$par,
          x = x,
          z = normalize(x))
 }
@@ -146,34 +148,70 @@ str(df)
 
     summary(lm(mpg ~ hp + wt + disp + drat + qsec))
 
-    response <- mpg
-    predictors <- list(hp, wt, disp, drat, qsec)
+    # setup
+    response <- qsec
+    predictors <- list(mpg, hp, wt)
 
-    prm <- rep(c(0, 1), 2 * (1 + length(predictors)))
-    last <- 1
+    # initialization
+    init <- FALSE
+    if (init) {
+        prm <- unname(c(zjohnson(response)$par, unlist(lapply(predictors, function(x) zjohnson(x)$par))))
+    } else {
+        prm <- rep(c(0, 1, 0, 1), 1 + length(predictors))
+    }
+
+    # optimization
+    last <- NULL
     better <- TRUE
     while (better) {
         opt <- optim(prm, function(prm) {
-            y <- qnorm(pjohnson(response, prm[1], prm[2], prm[3], prm[4]))
+            y <- gasinh(response, prm[1], prm[2], prm[3], prm[4])
             x <- rowSums(sapply(1:length(predictors), function(k) {
-                qnorm(pjohnson(predictors[[k]], prm[4 * k + 1], prm[4 * k + 2], prm[4 * k + 3], prm[4 * k + 4]))
+                gasinh(predictors[[k]], prm[4 * k + 1], prm[4 * k + 2], prm[4 * k + 3], prm[4 * k + 4])
             }))
             tryCatch({
                 m <- lm(y ~ x)
-                summary(m)$coefficients[2, 4]
+                -summary(m)$fstatistic[1]
             }, error = function(e) {
                 Inf
             })
         })
 
         prm <- opt$par
-        print(opt$value / last)
-        if (0.999 < (opt$value / last)) {
+        print(c(last / opt$value, opt$value, last))
+        if (is.null(last)) {
+            last <- opt$value
+        } else if (0.999 < (last / opt$value)) {
             better <- FALSE
         }
         last <- opt$value
     }
+
+    y <- m$fitted.values
+    # y <- gamma + delta * asinh((x - xi) / lambda)
+    y_hat <- sinh((m$fitted.values - prm[1]) / prm[2]) * prm[4] + prm[3]
+    ratio <- response / y_hat
+    round(cbind(mtcars[, c(1, 4, 6, 7)], y_hat = y_hat, ratio = ratio), 2)[order(ratio), ]
+
 }
+
+with(mtcars, {
+    pdf("nlj.pdf", 9, 6)
+
+    plot(mpg, qsec, pch = 16)
+    segments(mpg, qsec, mpg, y_hat, pch = 16, col = "red")
+    text(mpg, qsec, 1:nrow(mtcars), pos = 3)
+
+    plot(hp, qsec, pch = 16)
+    segments(hp, qsec, hp, y_hat, pch = 16, col = "red")
+    text(hp, qsec, 1:nrow(mtcars), pos = 3)
+
+    plot(wt, qsec, pch = 16)
+    segments(wt, qsec, wt, y_hat, pch = 16, col = "red")
+    text(wt, qsec, 1:nrow(mtcars), pos = 3)
+
+    dev.off()
+})
 
 with(df, {
     pdf("nlj.pdf", 9, 6)
